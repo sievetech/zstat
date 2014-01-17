8035a76f815c8340e67f32570ca59fc1072a72cc
Dalton Barreto <daltonmatos@sieve.com.br>
2014-01-17 19:19:39 -0200

    Version bump: 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zstat',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(),
     entry_points={
             'console_scripts': [

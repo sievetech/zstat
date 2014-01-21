945ecdd930daba8925a752bff3e6c149aa5a4ac6
Dalton Barreto <daltonmatos@sieve.com.br>
2014-01-21 18:03:46 -0200

    Get message total count: Server and per queue

---
+++ tests/rabbitmq_module_test.py
@@ -0,0 +1,37 @@
+# coding=utf-8
+import unittest
+import mock
+
+from zstat.modules import rabbitmq
+
+
+class MysqlModuleTest(unittest.TestCase):
+
+    def setUp(self):
+        self._get_client_patch = mock.patch.object(rabbitmq, '_get_client')
+        self._get_client_patch.start()
+        self.client_mock = mock.MagicMock()
+        rabbitmq._get_client.return_value = self.client_mock
+
+    def tearDown(self):
+        self._get_client_patch.stop()
+
+    def test_get_rabbitmq_total_msg_without_vhost(self):
+        self.client_mock.get_overview.return_value = {u'queue_totals': {u'messages': 21294,
+                                                                        u'messages_ready': 20436,
+                                                                        u'messages_unacknowledged': 858
+                                                                        }
+                                                      }
+        value = rabbitmq.rabbitmq_totalmsg()
+        self.assertEqual(21294, value)
+
+
+    def test_get_rabbitmq_total_msg_one_queue(self):
+        self.client_mock.get_overview.return_value = {u'queue_totals': {u'messages': 21294,
+                                                                        u'messages_ready': 20436,
+                                                                        u'messages_unacknowledged': 858
+                                                                        }
+                                                      }
+        self.client_mock.get_queue.return_value = {"messages": 42}
+        value = rabbitmq.rabbitmq_totalmsg("/", "extractor")
+        self.assertEqual(42, value)
--- zstat/modules/rabbitmq.py
+++ zstat/modules/rabbitmq.py
@@ -1,1 +1,21 @@
 # coding=utf-8
+from pyrabbit.api import Client
+from zstat.settings import config
+
+rabbit_options = config.get("rabbitmq", {})
+
+host = rabbit_options.get("host", "localhost")
+port = rabbit_options.get("port", "55672")
+user = rabbit_options.get("user", "guest")
+passwd = rabbit_options.get("pwd", "guest")
+
+
+def _get_client():
+    return Client("{}:{}".format(host, port), user, passwd)
+
+
+def rabbitmq_totalmsg(*args):
+    client = _get_client()
+    if args and len(args) == 2:  # Host and queue-name
+        return client.get_queue(args[0], args[1])['messages']
+    return client.get_overview()['queue_totals']['messages']
